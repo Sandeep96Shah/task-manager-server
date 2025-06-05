aeb6a42e83a3626ebb53b0cb2fb4302f21fb458a
Sandeep Shah <sandeep2016shah@gmail.com>
2025-06-05 17:41:48 +0600

    added auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,21 +10,132 @@ const generateToken = (userId) => {
 // @desc Register a new user
 // @route POST /api/auth/register
 // @access Public
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl, adminInviteToken } =
+      req.body;
+
+    //  check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ message: "User already Exist" });
+    }
+
+    // Determine user role: admin if correct toke is provided else member
+    let role = "member";
+    if (
+      adminInviteToken &&
+      adminInviteToken === process.env.ADMIN_INVITE_TOKEN
+    ) {
+      role = "admin";
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create User
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
+      role,
+    });
+
+    //  Return response
+    res.status(200).json({
+      _id: user._id,
+      email: user.email,
+      role: user.role,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
 
 // @desc Login user
 // @route POST /api/auth/login
 // @access Public
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(401).json({ message: "Invalid Email/Password" });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(401).json({ message: "Invalid Email/Password" });
+    }
+
+    //  Return response
+    res.status(200).json({
+      _id: user._id,
+      email: user.email,
+      role: user.role,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
 
 // @desc Get user profile
 // @route GET /api/auth/profile
 // @access Private (Requires JWT)
-const getUserProfile = async (req, res) => {};
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      res.status(404).json({ message: "User Not Found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
 
 // @desc Update user profile
 // @route PUT /api/auth/profile
 // @access Private (Requires JWT)
-const updateUserProfile = async (req, res) => {};
+const updateUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(req.body.password, salt);
+    }
+
+    const updatedUser = await user.save();
+
+    // Return Response
+    return res.status(200).json({
+      _id: updatedUser._id,
+      email: updatedUser.email,
+      role: updatedUser.role,
+      profileImageUrl: updatedUser.profileImageUrl,
+      token: generateToken(updatedUser._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
 
 module.exports = { registerUser, loginUser, getUserProfile, updateUserProfile };
