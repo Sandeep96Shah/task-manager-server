cae4c464aa956096373989b68bc607c5e629ad4d
Sandeep Shah <sandeep2016shah@gmail.com>
2025-06-05 16:21:11 +0600

    base setup

---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// middleware to handle cors
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "PUT", "POST", "DELETE"],
+  })
+);
+
+// Connect DataBase
+connectDB();
+
+// middleware
+app.use(express.json());
+
+// Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/reports", reportReports);
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})
